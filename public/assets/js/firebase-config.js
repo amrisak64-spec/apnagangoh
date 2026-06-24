01069dee2769516b787549486a7226a09c9e7c44
unknown <amrisak74@gmail.comm>
2026-06-24 20:32:57 +0530

    Add firebase config for deployment

---
+++ public/assets/js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC6BTw0M1FrY4O7kQfeRe8MkyJnfCddOjs",
+  authDomain: "apnagangoh-web.firebaseapp.com",
+  projectId: "apnagangoh-web",
+  storageBucket: "apnagangoh-web.firebasestorage.app",
+  messagingSenderId: "985679869840",
+  appId: "1:985679869840:web:f41a9ec4f33c31544b2515"
+};
